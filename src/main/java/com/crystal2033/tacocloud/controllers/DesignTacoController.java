95d45f2d6ad3622e99512a0e6604226f5657674f
Pavel <kulikov-pavlik@bk.ru>
2023-07-17 18:13:57 +0300

    Made validation

--- src/main/java/com/crystal2033/tacocloud/controllers/DesignTacoController.java
+++ src/main/java/com/crystal2033/tacocloud/controllers/DesignTacoController.java
@@ -4,9 +4,11 @@ import com.crystal2033.tacocloud.models.Ingredient;
 import com.crystal2033.tacocloud.models.Ingredient.Type;
 import com.crystal2033.tacocloud.models.Taco;
 import com.crystal2033.tacocloud.models.TacoOrder;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -68,7 +70,10 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processTaco(Taco taco, @ModelAttribute TacoOrder tacoOrder){
+    public String processTaco(@Valid Taco taco, Errors errors, @ModelAttribute TacoOrder tacoOrder){
+        if(errors.hasErrors()){
+            return "design";
+        }
         tacoOrder.addTaco(taco);
         log.info("Processing taco: {}", taco);
         return "redirect:/orders/current";
--- src/main/java/com/crystal2033/tacocloud/controllers/OrderController.java
+++ src/main/java/com/crystal2033/tacocloud/controllers/OrderController.java
@@ -1,8 +1,10 @@
 package com.crystal2033.tacocloud.controllers;
 
 import com.crystal2033.tacocloud.models.TacoOrder;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,8 +29,12 @@ public class OrderController {
     }
 
     @PostMapping
-    public String processOrder(TacoOrder order,
+    public String processOrder(@Valid TacoOrder order, Errors errors,
                                SessionStatus sessionStatus) {
+        if(errors.hasErrors()){
+            return "orderForm";
+        }
+
         log.info("Order submitted: {}", order);
         sessionStatus.setComplete();
         return "redirect:/";
